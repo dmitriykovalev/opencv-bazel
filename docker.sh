281e7f2b1349647412089e4e889a2b37a20848c6
Dmitriy Kovalev <dmitriy.kovalev@gmail.com>
2021-07-20 15:58:26 +0300

    Run any command using docker.sh

--- docker.sh
+++ docker.sh
@@ -9,5 +9,7 @@ fi
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly DOCKER_FILE=${DOCKER_FILE:-"${SCRIPT_DIR}/docker/Dockerfile"}
 readonly DOCKER_IMAGE=${DOCKER_IMAGE:-debian:buster}
-readonly DOCKER_IMAGE_TAG=$(docker build --build-arg IMAGE="${DOCKER_IMAGE}" --quiet - < "${DOCKER_FILE}")
-docker run -it --rm -v $(pwd):/workspace -w /workspace "${DOCKER_IMAGE_TAG}" /bin/bash
+readonly DOCKER_IMAGE_TAG=$(docker build --build-arg IMAGE="${DOCKER_IMAGE}" \
+                                         --quiet - < "${DOCKER_FILE}")
+
+docker run -it --rm -v $(pwd):/workspace -w /workspace "${DOCKER_IMAGE_TAG}" "$@"
